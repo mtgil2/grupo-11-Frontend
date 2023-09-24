71dba4e03dfcac87349764933512a1363195e89d
Miguel Luengo <miguel.luengo@uc.cl>
2023-09-24 00:12:42 -0300

    JWT Header WIP

--- src/views/Profile.jsx
+++ src/views/Profile.jsx
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { Container, Row, Col } from "reactstrap";
-
+import NumericInput from "../components/NumericInput";
 import Highlight from "../components/Highlight";
 import Loading from "../components/Loading";
 import { useAuth0, withAuthenticationRequired } from "@auth0/auth0-react";
+import BusinessButton from "../components/BusinessButton";
+import HistoryButton from "../components/HistoryButton";
+import StocksButton from "../components/StocksButton";
 
 export const Profile = () => {
   const { user, isAuthenticated, getAccessTokenSilently, getIdTokenClaims } = useAuth0();
@@ -12,39 +15,37 @@ export const Profile = () => {
   useEffect(() => {
     const getUserMetadata = async () => {
       const domain = "dev-jgor463dhotlvfgo.us.auth0.com";
-  
       try {
         const accessToken = await getIdTokenClaims();
         console.log(accessToken)
-       //const accessToken = await getAccessTokenSilently({
-       //   authorizationParams: {
-       //     audience: `https://${domain}/api/v2/`,
-       //     scope: "read:current_user",
-       //   },
-       // });
-  
-        const userDetailsByIdUrl = `https://${domain}/api/v2/users/${user.sub}`
-  
-        const metadataResponse = await fetch(userDetailsByIdUrl, {
+
+        const jsonResponse = JSON.parse(accessToken);
+        const jwt = jsonResponse.__raw;
+
+        fetch(`https://${domain}/api/v2/)`, {
+          method: 'GET',
           headers: {
-            Authorization: `Bearer ${accessToken}`,
-          },
-        });
-  
-        const { user_metadata } = await metadataResponse.json();
-  
-        setUserMetadata(user_metadata);
+            'Authorization': `Bearer ${jwt}`
+          }
+        })
+        .then(response => {
+          if (!response.ok) {
+            throw new Error('Network response failed');
+          }
+          return response.json();
+        })
+        .then(data => console.log(data))
+        .catch(error => console.error('Error:', error))
+
       } catch (e) {
         console.log(e.message);
       }
     };
   
     getUserMetadata();
-  }, [getAccessTokenSilently]);
-  if (user) {
-    console.log(user)
-}
+  });
 
+  console.log(user.user_metadata);
   return (
     <Container className="mb-5">
       <Row className="align-items-center profile-header mb-5 text-center text-md-left">
@@ -54,24 +55,16 @@ export const Profile = () => {
             alt="Profile"
             className="rounded-circle img-fluid profile-picture mb-3 mb-md-0"
           />
+          <h2>Bienvenid@, {user.name}</h2>
         </Col>
         <Col md>
-          <h2>{user.name}</h2>
-          <p className="lead text-muted">{user.email}</p>
-          <h3>User Metadata</h3>
-          {userMetadata ? (
-            <pre>{JSON.stringify(userMetadata, null, 2)}</pre>
-          ) : (
-            "No user metadata defined"
-          ) }
+          <p>Ahora mismo tienes {user.user_metadata} pesos en tu cuenta.</p>
+          <NumericInput/>
         </Col>
       </Row>
-      <Row>
-        WE GETTING MONEY
-      </Row>
-      <a href='/empresas'>Ir a Empresas</a>
-      <a href='/historia'>Ir a Historia</a>
-      <a href='/acciones'>Ir a Acciones</a>
+      <BusinessButton/>
+      <HistoryButton/>
+      <StocksButton/>
     </Container>
   );
 };
